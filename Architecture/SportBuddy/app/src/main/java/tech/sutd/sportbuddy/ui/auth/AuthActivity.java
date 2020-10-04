5f80180202d89911f33193554051ce76a952d797
yuanhawk <yuanmirage@yahoo.com>
2020-10-04 21:58:35 +0800

    dagger skeleton

--- Architecture/SportBuddy/app/src/main/java/tech/sutd/sportbuddy/ui/auth/AuthActivity.java
+++ Architecture/SportBuddy/app/src/main/java/tech/sutd/sportbuddy/ui/auth/AuthActivity.java
@@ -1,16 +1,19 @@
 package tech.sutd.sportbuddy.ui.auth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import dagger.android.support.DaggerAppCompatActivity;
 import tech.sutd.sportbuddy.R;
+import tech.sutd.sportbuddy.databinding.ActivityAuthBinding;
 
-public class AuthActivity extends AppCompatActivity {
+public class AuthActivity extends DaggerAppCompatActivity {
+
+    private ActivityAuthBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityAuthBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
