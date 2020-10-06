e6bfcbb8e6e5b4afd4c436af0568128a957aa3cc
yuanhawk <yuanmirage@yahoo.com>
2020-10-07 00:46:23 +0800

    initial login

--- Architecture/PUG/app/src/main/java/tech/sutd/pickupgame/di/AppModule.java
+++ Architecture/PUG/app/src/main/java/tech/sutd/pickupgame/di/AppModule.java
@@ -1,11 +1,20 @@
 package tech.sutd.pickupgame.di;
 
+import android.app.Application;
+import android.graphics.drawable.Drawable;
+
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.database.FirebaseDatabase;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import tech.sutd.pickupgame.R;
 
 @Module
 public class AppModule {
@@ -16,4 +25,24 @@ public class AppModule {
         return FirebaseDatabase.getInstance();
     }
 
+    @Singleton
+    @Provides
+    static RequestOptions provideRequestOptions() {
+        return RequestOptions
+                .placeholderOf(R.drawable.white_background)
+                .error(R.drawable.white_background);
+    }
+
+    @Singleton
+    @Provides
+    static RequestManager provideGlideInstance(Application application, RequestOptions requestOptions) {
+        return Glide.with(application)
+                .setDefaultRequestOptions(requestOptions);
+    }
+
+    @Singleton
+    @Provides
+    static Drawable provideLoginDrawable(Application application) {
+        return ContextCompat.getDrawable(application, R.drawable.pug);
+    }
 }
--- Architecture/PUG/app/src/main/java/tech/sutd/pickupgame/ui/auth/AuthActivity.java
+++ Architecture/PUG/app/src/main/java/tech/sutd/pickupgame/ui/auth/AuthActivity.java
@@ -2,7 +2,13 @@ package tech.sutd.pickupgame.ui.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.RequestManager;
+
+import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 import tech.sutd.pickupgame.R;
@@ -12,10 +18,23 @@ public class AuthActivity extends DaggerAppCompatActivity {
 
     private ActivityAuthBinding binding;
 
+    @Inject
+    Drawable logo;
+
+    @Inject
+    RequestManager requestManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAuthBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        setLogo();
+    }
+
+    private void setLogo() {
+        requestManager.load(logo)
+                .into((ImageView) binding.loginImg);
     }
 }
