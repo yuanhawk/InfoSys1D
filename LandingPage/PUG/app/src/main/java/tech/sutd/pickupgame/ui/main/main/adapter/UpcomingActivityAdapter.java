9918fa3a3fdfb3f2bf68613b54df0cf11689fb09
yuanhawk <yuanmirage@yahoo.com>
2020-10-29 15:28:21 +0800

    updated font + cardview

--- LandingPage/PUG/app/src/main/java/tech/sutd/pickupgame/ui/main/main/adapter/UpcomingActivityAdapter.java
+++ LandingPage/PUG/app/src/main/java/tech/sutd/pickupgame/ui/main/main/adapter/UpcomingActivityAdapter.java
@@ -16,67 +16,41 @@ import tech.sutd.pickupgame.databinding.ItemlistUpcomingActivitiesBinding;
 import tech.sutd.pickupgame.models.ui.Friends;
 import tech.sutd.pickupgame.models.ui.UpcomingActivity;
 
-public class UpcomingActivityAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class UpcomingActivityAdapter extends RecyclerView.Adapter<UpcomingActivityAdapter.ViewHolder> {
 
-    public static final int LAYOUT_ONE = 0;
-    public static final int LAYOUT_TWO = 1;
-
-    private List upcomingActivities = new ArrayList();
+    private List<UpcomingActivity> upcomingActivities = new ArrayList<>();
     private RequestManager requestManager;
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        switch (getItemViewType(viewType)) {
-            case LAYOUT_ONE:
-                ItemlistUpcomingActivitiesBinding upcomingBind = ItemlistUpcomingActivitiesBinding.inflate(inflater, parent, false);
-                return new UpcomingActViewHolder(upcomingBind);
-            case LAYOUT_TWO:
-                ItemlistFriendsBinding friendsBinding = ItemlistFriendsBinding.inflate(inflater, parent, false);
-                return new FriendsViewHolder(friendsBinding);
-        }
-        return null;
+        ItemlistUpcomingActivitiesBinding binding = ItemlistUpcomingActivitiesBinding.inflate(inflater, parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        switch (getItemViewType(position)) {
-            case LAYOUT_ONE:
-                UpcomingActivity upcomingActivities = (UpcomingActivity) this.upcomingActivities.get(position);
-                UpcomingActViewHolder holder1 = (UpcomingActViewHolder) holder;
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        UpcomingActivity upcomingActivity = upcomingActivities.get(position);
 
-                holder1.binding.sport.setText(upcomingActivities.getSport());
-                holder1.binding.time.setText(upcomingActivities.getClock());
-                holder1.binding.location.setText(upcomingActivities.getLocation());
-                holder1.binding.organizer.setText(upcomingActivities.getOrganizer());
+        holder.binding.sport.setText(upcomingActivity.getSport());
+        holder.binding.time.setText(upcomingActivity.getClock());
+        holder.binding.location.setText(upcomingActivity.getLocation());
+        holder.binding.organizer.setText(upcomingActivity.getOrganizer());
 
-                requestManager.load(upcomingActivities.getClockImg())
-                        .into(holder1.binding.clockImg);
-                requestManager.load(upcomingActivities.getLocationImg())
-                        .into(holder1.binding.locationImg);
-                requestManager.load(upcomingActivities.getOrganizerImg())
-                        .into(holder1.binding.organizerImg);
-                requestManager.load(upcomingActivities.getSportImg())
-                        .into(holder1.binding.sportImg);
-                break;
-            case LAYOUT_TWO:
-                Friends newActivities = (Friends) this.upcomingActivities.get(position);
-                ((FriendsViewHolder) holder).binding.friends.setText(newActivities.getName());
-                break;
-        }
+        requestManager.load(upcomingActivity.getClockImg())
+                .into(holder.binding.clockImg);
+        requestManager.load(upcomingActivity.getLocationImg())
+                .into(holder.binding.locationImg);
+        requestManager.load(upcomingActivity.getOrganizerImg())
+                .into(holder.binding.organizerImg);
+        requestManager.load(upcomingActivity.getSportImg())
+                .into(holder.binding.sportImg);
     }
 
     @Override
     public int getItemCount() {
         return upcomingActivities.size();
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        if (UpcomingActivity.class.isInstance(upcomingActivities.get(position)))
-            return 0;
-        return 1;
     }
 
     public void setNotifications(List<UpcomingActivity> upcomingActivities, RequestManager requestManager) {
@@ -85,21 +59,11 @@ public class UpcomingActivityAdapter extends RecyclerView.Adapter<RecyclerView.V
         notifyDataSetChanged();
     }
 
-    public static class FriendsViewHolder extends RecyclerView.ViewHolder {
-
-        private ItemlistFriendsBinding binding;
-
-        public FriendsViewHolder(@NonNull ItemlistFriendsBinding binding) {
-            super(binding.getRoot());
-            this.binding = binding;
-        }
-    }
-
-    public static class UpcomingActViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
 
         private ItemlistUpcomingActivitiesBinding binding;
 
-        public UpcomingActViewHolder(@NonNull ItemlistUpcomingActivitiesBinding binding) {
+        public ViewHolder(@NonNull ItemlistUpcomingActivitiesBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
         }
