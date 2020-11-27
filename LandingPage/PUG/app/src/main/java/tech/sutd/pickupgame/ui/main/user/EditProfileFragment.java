75ab1a448a5509fb651880fc928e4a9a4996c8ac
flippinghell <39935719+flippinghell@users.noreply.github.com>
2020-11-27 15:55:12 +0800

    forgot to commit edit profile layout

---
+++ LandingPage/PUG/app/src/main/java/tech/sutd/pickupgame/ui/main/user/EditProfileFragment.java
@@ -0,0 +1,34 @@
+package tech.sutd.pickupgame.ui.main.user;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import dagger.android.support.DaggerFragment;
+import tech.sutd.pickupgame.databinding.FragmentUserBinding;
+
+public class EditProfileFragment extends DaggerFragment {
+
+    private FragmentUserBinding binding;
+
+    private NavController navController;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentUserBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+    }
+}
